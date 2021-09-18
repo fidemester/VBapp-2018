1f1062b9b045ca240822f2a29e976440b741a925
Krisztian <fodorkrisztian6@gmail.com>
2021-09-18 13:47:41 +0200

    basic function vol2

--- snake.js
+++ snake.js
@@ -5,6 +5,28 @@ const Component = {};
 
 // Irány billentyűk megadása
 Keyboard.Keymap = {
-    37: 'left'
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+};
+
+// Billenytű események
+Keyboard.ControllerEvents = function() {
+    const self = this;
+    this.pressKey = null;
+    this.keymap = Keyboard.Keymap;
+
+    // Billentyű lenyomás figyelése
+    document.addEventListener ('keydown', (e) => {
+        const key = e.keyCode;
+    console.log("pressKey", key);
+     })
+    // Az utoljára lenyomott billenytű lekérése
+
+    this.getKey = function() {
+        return this.keymap[this.pressKey]
+    }
 }
+const ce = new Keyboard.ControllerEvents();
 
